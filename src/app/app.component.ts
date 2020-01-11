205a811b3073ed66b9fbb6a08e3078eec150ad16
Acer <mukundpatel1993@gmail.com>
2020-01-11 11:24:22 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { GlobalService } from './global/global.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  
-  constructor(public GLF: GlobalService){
-  }
-  public cockpit = { height:0};
-  @ViewChild('passCabin') PassCabin ;
-  ngAfterViewInit(): void {
-    setTimeout(()=>{
-      this.cockpit.height =  this.PassCabin.nativeElement.clientHeight;
-      console.log( this.PassCabin.nativeElement.clientHeight)
-    },0)
-   
-  }
+export class AppComponent {
+  title = 'testang';
 }
